fc40eac236809c458d488b115d3e893515002de4
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-08-20 13:31:03 +0530

    added pagination links for different pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql, StaticQuery } from "gatsby"
 import Post from "../components/Post"
+import PaginationLinks from '../components/PaginationLinks'
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const postPerPage = 2
+  let numberOfPages
+  return(
   <Layout pageTitle = "SagarBlog">
-    <SEO title="Home" />
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <StaticQuery query={indexQuery} render={data => {
+        numberOfPages = Math.ceil(data.allMarkdownRemark.totalCount / postPerPage)
       return(
         <div>
           {data.allMarkdownRemark.edges.map(({ node }) => (
             <Post  
-            key={node.id}
+            key={node.id }
             title={node.frontmatter.title}
             author={node.frontmatter.author}
             slug={node.fields.slug}
@@ -23,18 +27,21 @@ const IndexPage = () => (
             tags={node.frontmatter.tags}
             />
           ))}
+          <PaginationLinks currentPage={1} numberOfPages={numberOfPages}/>
         </div>
       )
     }}
     />
   </Layout>
-)
+  )
+}
 
 const indexQuery = graphql`
 query{
   allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }
     limit: 2
     ) {
+    totalCount
     edges{
       node{
         id
--- src/templates/post-list.js
+++ src/templates/post-list.js
@@ -2,60 +2,65 @@ import React from 'react'
 import Layout from '../components/layout'
 import Post from '../components/Post'
 import { graphql } from 'gatsby'
+import PaginationLinks from '../components/PaginationLinks'
 
 const postList = props => {
-    const posts = props.data.allMarkdownRemark.edges
-    const { currentPage } = props.pageContext
+  const posts = props.data.allMarkdownRemark.edges
+  const { currentPage, numberOfPages } = props.pageContext
 
-    return (
-        <Layout pageTitle={`Page: ${currentPage}`}>
-            {posts.map(({ node }) => (
-                <Post key={node.id}
-                slug={node.fields.slug}
-                title={node.frontmatter.title}
-                author={node.frontmatter.author}
-                date= {node.frontmatter.date}
-                body={node.excerpt}
-                tags={node.frontmatter.tags}
-                fluid={node.frontmatter.images.childImageSharp.fluid}
-                />
-            ))}
-        </Layout>
-    )
-
+  return (
+    <Layout pageTitle={`Page: ${currentPage}`}>
+      {posts.map(({ node }) => (
+        <Post
+          key={node.id}
+          slug={node.fields.slug}
+          title={node.frontmatter.title}
+          author={node.frontmatter.author}
+          date={node.frontmatter.date}
+          body={node.excerpt}
+          tags={node.frontmatter.tags}
+          fluid={node.frontmatter.images.childImageSharp.fluid}
+        />
+      ))}
+      <PaginationLinks
+        currentPage={currentPage}
+        numberOfPages={numberOfPages}
+      />
+    </Layout>
+  )
 }
 
 export const postListQuery = graphql`
-    query postListQuery($skip: Int!, $limit: Int!){
-        allMarkdownRemark(
-            sort: { fields: [frontmatter___date], order: DESC}
-            limit: $limit
-            skip: $skip
-        ){
-            edges{
-                node{
-                    id
-                    frontmatter{
-                        title
-                        date(formatString: "MMMM Do YYYY")
-                        author
-                        tags
-                        images{
-                            childImageSharp{
-                                fluid(maxWidth: 650, maxHeight: 370){
-                                    ...GatsbyImageSharpFluid
-                                }
-                            }
-                        }
-                    }
-                    fields{
-                        slug
-                    }
-                    excerpt
+  query postListQuery($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM Do YYYY")
+            author
+            tags
+            images {
+              childImageSharp {
+                fluid(maxWidth: 650, maxHeight: 371) {
+                  ...GatsbyImageSharpFluid
                 }
+              }
             }
+          }
+          fields {
+            slug
+          }
+          excerpt
         }
+      }
     }
+  }
 `
 
 export default postList
