94b98b13fa9eec440b17e02f45a2bb4e4580928d
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-08-30 18:59:38 +0530

    Added text color and image in author page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import swapnil from '../images/swapnil.jpg'
 
 const AboutPage = () => (
   <Layout pageTitle="About Me">
     <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
     <div>
-    <p >
-      Hi, I am Swapnil Sagar.<br></br>
+    <div className="col-md-6">
+        <img src={swapnil} style={{ maxWidth: '80%' }} alt="Swapnil profile"/>
+      </div>
+      <div className="col-md-6">
+    <p style={{ color: '#FFFF'}} >
+    <br></br>Hi, I am Swapnil Sagar.<br></br>
 
-      I am a Final year Computer Science and Engneering Student whose passion is to learn and code new stuffs.
-      I run on Caffeine and I conquer bugs.<br></br>
+    <br></br>I am a Final year Computer Science and Engneering Student whose passion is to learn and code new stuffs.<br></br>
+      <br></br>I run on Caffeine and I conquer bugs.<br></br>
 
-      My obejctive is to pursue my career in an organization with performance-oriented environment for achievement of personal advancement. Seeking for an entry-level position to begin my career in a high-level professional environment.
-    </p></div>
+      <br></br> My obejctive is to pursue my career in an organization with performance-oriented environment for achievement of personal advancement. Seeking for an entry-level position to begin my career in a high-level professional environment.
+    </p>
+    </div></div>
   </Layout>
 )
 
